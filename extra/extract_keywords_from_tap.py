6bab996ad11d6dfabbaebaa7426ed70e2ba54cb4
Benjamin Mampaey <benjamin.mampaey@oma.be>
2023-11-16 15:47:59 +0100

    Improved extract_keywords_from_tap

--- extra/extract_keywords_from_tap.py
+++ extra/extract_keywords_from_tap.py
@@ -26,20 +26,36 @@ class KeywordInspector:
 	}
 	
 	def __init__(self, url, exclude_columns = []):
-		service = pyvo.dal.TAPService(url)
+		self.service = pyvo.dal.TAPService(url)
 		table_name = self.resolve_ambiguity(
 			'Select the table to inspect',
-			[table.name for table in service.tables]
+			[table.name for table in self.service.tables]
 		)
-		self.table = service.tables[table_name]
+		self.table = self.service.tables[table_name]
 		self.exclude_columns = exclude_columns
 		logging.info('Table description:\n%s\n', self.table.description)
 	
+	def get_sample_record(self):
+		'''Get the first record from the table to use as sample'''
+		query = 'SELECT TOP 1 * FROM %s' % self.table.name
+	
+		logging.debug('Executing TAP query "%s"', query)
+		try:
+			records = self.service.search(query)
+		except Exception as why:
+			logging.warning('TAP query "%s" failed : %s', query, why)
+			raise
+		
+		return records[0]
+
 	def get_keyword_infos(self):
 		'''Return a list of all keyword info {name, verbose_name, type, unit, description} resolving ambiguities by aking the user'''
 		
 		keyword_infos = list()
 		
+		# Get a sample value to help user resolve ambiguities in keyword type
+		sample_record = self.get_sample_record()
+
 		for column in self.table.columns:
 			
 			if column.name in self.exclude_columns:
@@ -48,7 +64,7 @@ class KeywordInspector:
 				keyword_infos.append({
 					'name': self.get_keyword_name(column),
 					'verbose_name': self.get_keyword_verbose_name(column),
-					'type': self.get_keyword_type(column),
+					'type': self.get_keyword_type(column, sample_record.get(column.name)),
 					'unit': self.get_keyword_unit(column),
 					'description': self.get_keyword_description(column),
 				})
@@ -75,7 +91,7 @@ class KeywordInspector:
 		'''Extract the keyword verbose name from the column definition'''
 		return column.name
 	
-	def get_keyword_type(self, column):
+	def get_keyword_type(self, column, sample_value):
 		'''Extract the keyword type from the column definition'''
 		
 		# Inspect the column datatype to infer the SVO keyword type equivalant
@@ -85,19 +101,19 @@ class KeywordInspector:
 		
 		if keyword_type is None:
 			keyword_type = self.resolve_ambiguity(
-				'Column "{column.name}" datatype "{column.datatype.content}" is not supported, select appropriate SVO keyword type'.format(column=column),
+				'Column "{column.name}" datatype "{column.datatype.content}" is not supported, select appropriate SVO keyword type\nUnit: {unit}\nSample value: {sample_value}'.format(column=column, unit = self.get_keyword_unit(column), sample_value = sample_value),
 				['text', 'boolean', 'integer', 'real', 'time (ISO 8601)']
 			)
 		
 		if keyword_type != 'time (ISO 8601)' and 'time' in column.ucd:
 			keyword_type = self.resolve_ambiguity(
-				'Column "{column.name}" has ucd "{column.ucd}", select appropriate SVO keyword type'.format(column=column),
+				'Column "{column.name}" has ucd "{column.ucd}", select appropriate SVO keyword type\nUnit: {unit}\nSample value: {sample_value}'.format(column=column, unit = self.get_keyword_unit(column), sample_value = sample_value),
 				['time (ISO 8601)', keyword_type]
 			)
 		
 		if 'x' in column.datatype.arraysize or (column.datatype.arraysize != '1' and keyword_type not in ['text', 'time (ISO 8601)']):
 			keyword_type = self.resolve_ambiguity(
-				'Column "{column.name}" datatype "{column.datatype.content}" with arraysize {column.datatype.arraysize} is not supported, select appropriate SVO keyword type'.format(column=column),
+				'Column "{column.name}" datatype "{column.datatype.content}" with arraysize {column.datatype.arraysize} is not supported, select appropriate SVO keyword type\nUnit: {unit}\nSample value: {sample_value}'.format(column=column, unit = self.get_keyword_unit(column), sample_value = sample_value),
 				['text', 'boolean', 'integer', 'real', 'time (ISO 8601)']
 			)
 		
