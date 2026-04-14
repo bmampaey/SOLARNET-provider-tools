0ea37c00a9de2e3888b40f0046418d47811806fc
Benjamin Mampaey <benjamin.mampaey@oma.be>
2026-04-14 11:29:08 +0200

    Added scripts for dataset SPoCA Coronal Hole

---
+++ sidc_oma_be/submit_metadata_spoca_coronal_hole.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python3
+"""Script to extract metadata from the SPoCA Coronal Hole archive and submit it to the SOLARNET Virtual Observatory"""
+
+import argparse
+import logging
+import sys
+from datetime import timedelta
+from pathlib import Path
+
+# HACK to make sure the provider_tools package is findable
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+from provider_tools import (
+	DataLocationFromLocalFile,
+	MetadataFromFitsHeader,
+	ProviderFromLocalFitsFile,
+	RESTfulApi,
+	utils,
+)
+
+DATASET = 'SPoCA Coronal Hole'
+
+
+class DataLocation(DataLocationFromLocalFile):
+	# The base directory to build the default file_path
+	BASE_FILE_PATH = '/data/spoca/spoca4tap/rob_spoca_ch/ch_map'
+
+	# The base file URL to build the default file_url (must end with a /)
+	BASE_FILE_URL = 'https://spoca.oma.be/spoca4tap/rob_spoca_ch/ch_map/'
+
+	# The base thumbnail URL to build the default tumbnail_url, uses the fits2thumbnail service of the SVO to convert FITS to png
+	BASE_THUMBNAIL_URL = 'https://spoca.oma.be/spoca4tap/rob_spoca_ch/ch_map_overlay/'
+
+	def get_thumbnail_url(self):
+		"""Override to return the proper URL for the thumbnail"""
+		return self.BASE_THUMBNAIL_URL + Path(self.local_file).with_suffix('.png').name
+
+
+class Metadata(MetadataFromFitsHeader):
+	def get_field_date_beg(self):
+		return self.get_field_value('date_obs')
+
+	def get_field_date_end(self):
+		return self.get_field_value('date_obs')
+
+	# TODO is there a better value for this
+	def get_field_wavemin(self):
+		return None
+
+	def get_field_wavemax(self):
+		return None
+
+	def get_field_thrawar(self):
+		return False
+
+	def get_field_tracked(self):
+		return True
+
+
+class Provider(ProviderFromLocalFitsFile):
+	METADATA_CLASS = Metadata
+
+	DATA_LOCATION_CLASS = DataLocation
+
+	HDU_NAME_OR_INDEX = 1
+
+
+if __name__ == '__main__':
+	# Get the arguments
+	parser = argparse.ArgumentParser(description='Submit metadata from a FITS file to the SVO')
+	parser.add_argument(
+		'--verbose',
+		'-v',
+		choices=['DEBUG', 'INFO', 'ERROR'],
+		default='INFO',
+		help='Set the logging level (default is INFO)',
+	)
+	parser.add_argument(
+		'fits_files',
+		metavar='FITS FILE',
+		nargs='+',
+		help='A FITS file to submit to the SVO (also accept glob pattern)',
+	)
+	parser.add_argument(
+		'--auth-file',
+		'-a',
+		default='./.svo_auth',
+		help='A file containing the username (email) and API key separated by a colon of the owner of the metadata',
+	)
+	parser.add_argument(
+		'--dry-run',
+		'-f',
+		action='store_true',
+		help='Do not submit data but print what data would be submitted instead',
+	)
+	parser.add_argument(
+		'--min-modif-time',
+		'-m',
+		type=utils.parse_date_time_string,
+		help='Only submit file if the modification time is after that date',
+	)
+
+	args = parser.parse_args()
+
+	# Setup the logging
+	logging.basicConfig(
+		level=getattr(logging, args.verbose),
+		format='%(asctime)s %(levelname)-8s: %(message)s',
+	)
+
+	try:
+		provider = Provider(RESTfulApi(auth_file=args.auth_file, debug=args.verbose == 'DEBUG'), DATASET)
+	except Exception as error:
+		logging.critical('Could not create provider: %s', error)
+		raise
+
+	provider.submit_new_metadata(utils.iter_files(args.fits_files, args.min_modif_time), args.dry_run)
