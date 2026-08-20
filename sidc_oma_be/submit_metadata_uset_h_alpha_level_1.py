d23889653555b592e55ab6a98ca95d9df83ac93f
Benjamin Mampaey <benjamin.mampaey@oma.be>
2026-08-20 11:29:00 +0200

    Updated USET providers to use URL instead of file paths

--- sidc_oma_be/submit_metadata_uset_h_alpha_level_1.py
+++ sidc_oma_be/submit_metadata_uset_h_alpha_level_1.py
@@ -5,34 +5,32 @@ import argparse
 import logging
 import sys
 from datetime import timedelta
-from pathlib import Path
+from pathlib import Path, PurePosixPath
 
 # HACK to make sure the provider_tools package is findable
 sys.path.append(str(Path(__file__).resolve().parent.parent))
 from provider_tools import (
-	DataLocationFromLocalFile,
+	DataLocationFromUrl,
 	MetadataFromFitsHeader,
-	ProviderFromLocalFitsFile,
+	ProviderFromFitsUrl,
 	RESTfulApi,
 	utils,
 )
 
 DATASET = 'USET H-alpha level 1'
+BASE_FILE_URL = 'https://www.sidc.be/uset/data/cameras/output_img/FTS/L1c/USET_H-alpha/'
 
 
-class DataLocation(DataLocationFromLocalFile):
-	# The base directory to build the default file_path
-	BASE_FILE_PATH = '/mnt/solar1-uset/cameras/output_img/FTS/L1c/USET_H-alpha/'
-
+class DataLocation(DataLocationFromUrl):
 	# The base file URL to build the default file_url (must end with a /)
-	BASE_FILE_URL = 'https://www.sidc.be/uset/data/cameras/output_img/FTS/L1c/USET_H-alpha/'
+	BASE_FILE_URL = BASE_FILE_URL
 
 	# The base thumbnail URL to build the default tumbnail_url, uses the fits2thumbnail service of the SVO to convert FITS to png
 	BASE_THUMBNAIL_URL = 'https://www.sidc.be/uset/data/cameras/output_img/png/L1c_png_750_text/USET_H-alpha/'
 
 	def get_thumbnail_url(self):
 		# Construct the thumbnail URL from the filename
-		return self.BASE_THUMBNAIL_URL + str(Path(self.get_file_path()).with_suffix('.png'))
+		return self.BASE_THUMBNAIL_URL + str(PurePosixPath(self.get_file_path()).with_suffix('.png'))
 
 
 class Metadata(MetadataFromFitsHeader):
@@ -47,7 +45,9 @@ class Metadata(MetadataFromFitsHeader):
 		return 656.325
 
 
-class Provider(ProviderFromLocalFitsFile):
+class Provider(ProviderFromFitsUrl):
+	HEADER_SIZE = 2 * 2880
+
 	METADATA_CLASS = Metadata
 
 	DATA_LOCATION_CLASS = DataLocation
@@ -89,10 +89,11 @@ if __name__ == '__main__':
 		help='Path to a JSONL file to which the metadata will be written, instead of printed',
 	)
 	parser.add_argument(
-		'fits_files',
-		metavar='FITS FILE',
-		nargs='+',
-		help='Path to a FITS file to process (also accept glob pattern)',
+		'fits_urls',
+		metavar='URL',
+		nargs='*',
+		default=[BASE_FILE_URL],
+		help="Path to a FITS file to process (also accept apache style directory indexing, don't forget to end diretories URL with a slash)",
 	)
 	args = parser.parse_args()
 
@@ -105,7 +106,7 @@ if __name__ == '__main__':
 		logging.critical('Could not initialise provider: %s', error)
 		raise
 
-	items = utils.iter_files(args.fits_files, args.min_modif_time)
+	items = utils.iter_urls(args.fits_urls, extension='.FTS', min_modification_time=args.min_modif_time)
 
 	if args.output_file:
 		with open(args.output_file, 'wt') as output_file:
--- sidc_oma_be/submit_metadata_uset_white_light_level_1.py
+++ sidc_oma_be/submit_metadata_uset_white_light_level_1.py
@@ -5,34 +5,32 @@ import argparse
 import logging
 import sys
 from datetime import timedelta
-from pathlib import Path
+from pathlib import Path, PurePosixPath
 
 # HACK to make sure the provider_tools package is findable
 sys.path.append(str(Path(__file__).resolve().parent.parent))
 from provider_tools import (
-	DataLocationFromLocalFile,
+	DataLocationFromUrl,
 	MetadataFromFitsHeader,
-	ProviderFromLocalFitsFile,
+	ProviderFromFitsUrl,
 	RESTfulApi,
 	utils,
 )
 
 DATASET = 'USET White Light level 1'
+BASE_FILE_URL = 'https://www.sidc.be/uset/data/cameras/output_img/FTS/L1c/USET_White_Light/'
 
 
-class DataLocation(DataLocationFromLocalFile):
-	# The base directory to build the default file_path
-	BASE_FILE_PATH = '/mnt/solar1-uset/cameras/output_img/FTS/L1c/USET_White_Light/'
-
+class DataLocation(DataLocationFromUrl):
 	# The base file URL to build the default file_url (must end with a /)
-	BASE_FILE_URL = 'https://www.sidc.be/uset/data/cameras/output_img/FTS/L1c/USET_White_Light/'
+	BASE_FILE_URL = BASE_FILE_URL
 
 	# The base thumbnail URL to build the default tumbnail_url, uses the fits2thumbnail service of the SVO to convert FITS to png
 	BASE_THUMBNAIL_URL = 'https://www.sidc.be/uset/data/cameras/output_img/png/L1c_png_750_text/USET_White_Light/'
 
 	def get_thumbnail_url(self):
 		# Construct the thumbnail URL from the filename
-		return self.BASE_THUMBNAIL_URL + str(Path(self.get_file_path()).with_suffix('.png'))
+		return self.BASE_THUMBNAIL_URL + str(PurePosixPath(self.get_file_path()).with_suffix('.png'))
 
 
 class Metadata(MetadataFromFitsHeader):
@@ -40,7 +38,9 @@ class Metadata(MetadataFromFitsHeader):
 		return self.extract_field_value('date_beg') + timedelta(seconds=self.extract_field_value('xposure'))
 
 
-class Provider(ProviderFromLocalFitsFile):
+class Provider(ProviderFromFitsUrl):
+	HEADER_SIZE = 2 * 2880
+
 	METADATA_CLASS = Metadata
 
 	DATA_LOCATION_CLASS = DataLocation
@@ -82,10 +82,11 @@ if __name__ == '__main__':
 		help='Path to a JSONL file to which the metadata will be written, instead of printed',
 	)
 	parser.add_argument(
-		'fits_files',
-		metavar='FITS FILE',
-		nargs='+',
-		help='Path to a FITS file to process (also accept glob pattern)',
+		'fits_urls',
+		metavar='URL',
+		nargs='*',
+		default=[BASE_FILE_URL],
+		help="Path to a FITS file to process (also accept apache style directory indexing, don't forget to end diretories URL with a slash)",
 	)
 	args = parser.parse_args()
 
@@ -98,7 +99,7 @@ if __name__ == '__main__':
 		logging.critical('Could not initialise provider: %s', error)
 		raise
 
-	items = utils.iter_files(args.fits_files, args.min_modif_time)
+	items = utils.iter_urls(args.fits_urls, extension='.FTS', min_modification_time=args.min_modif_time)
 
 	if args.output_file:
 		with open(args.output_file, 'wt') as output_file:
