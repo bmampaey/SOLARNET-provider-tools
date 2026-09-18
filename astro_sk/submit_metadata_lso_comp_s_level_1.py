2a6de26fef33581100c6741956abbf5968d8530a
Benjamin Mampaey <benjamin.mampaey@oma.be>
2026-09-18 12:33:27 +0200

    Updated astro_sk script for Michal

--- astro_sk/submit_metadata_lso_comp_s_level_1.py
+++ astro_sk/submit_metadata_lso_comp_s_level_1.py
@@ -4,7 +4,7 @@
 import argparse
 import logging
 import sys
-from datetime import timedelta
+import tarfile
 from pathlib import Path
 
 from astropy.io import fits
@@ -15,9 +15,13 @@ from provider_tools import (
 	MetadataFromFitsHeader,
 	ProviderFromLocalFitsFile,
 	RESTfulApi,
-	utils,
 )
 
+# Inside the TAR archive, the script expects the FITS files to have these prefix and suffix, they will be removed from the FITS filename to create the OID
+FITS_FILE_PREFIX = 'lso_comp-s_lev1.0_'
+FITS_FILE_SUFFIX = '.fits.fz'
+
+# Dont change this
 DATASET = 'LSO CoMP-S level 1'
 
 
@@ -45,39 +49,56 @@ class Provider(ProviderFromLocalFitsFile):
 		return resource_data
 
 
-def list_items(fits_files, min_modif_time=None):
-	for fits_file in utils.iter_files(fits_files, min_modif_time):
-		with fits.open(fits_file) as hdus:
-			for i, hdu in enumerate(hdus):
-				if i == 0:
-					continue
-				else:
-					yield hdu.header, f'{Path(fits_file).name.removeprefix("lso_comp-s_lev1.0_").removesuffix(".fits.fz")}_{i}'
+# Scan through a tar archive (compressed or not) to extract FITS metadata
+# and yield for each HDU the header and the OID
+def list_items(tar_file_path):
+	with tarfile.open(tar_file_path, 'r:*') as tar:
+		for member in tar.getmembers():
+			fits_file_path = Path(member.name)
+			if not fits_file_path.name.endswith(FITS_FILE_SUFFIX):
+				logging.warning('Skipping TAR file %s because it is not ending with suffix %s', fits_file_path, FITS_FILE_SUFFIX)
+				continue
+
+			if not fits_file_path.name.startswith(FITS_FILE_PREFIX):
+				logging.warning('Skipping TAR file %s because it is not starting with prefix %s', fits_file_path, FITS_FILE_PREFIX)
+				continue
+
+			fits_file = tar.extractfile(member)
+
+			if fits_file is None:
+				logging.info('Skipping TAR entry %s, not a regular file', member.name)
+				continue
+
+			logging.info('Extracting metadata from FITS file %s', fits_file_path)
+
+			with fits.open(fits_file) as hdus:
+				for i, hdu in enumerate(hdus):
+					if 'DATE-BEG' not in hdu.header:
+						logging.warning(
+							'Skipping HDU %s of file %s, it does not contain the required metadata DATE-BEG', i, fits_file_path
+						)
+						continue
+					yield hdu.header, f'{fits_file_path.name[len(FITS_FILE_PREFIX) : -len(FITS_FILE_SUFFIX)]}_{i}'
 
 
 if __name__ == '__main__':
 	# Get the arguments
 	parser = argparse.ArgumentParser(
-		description='Extract metadata from FITS files and submit them to the SVO for dataset "%s"' % DATASET
+		description='Extract metadata from FITS files within a TAR archive and submit them to the SVO for dataset "%s"'
+		% DATASET
 	)
 	parser.add_argument(
 		'--verbose',
 		'-v',
-		choices=['DEBUG', 'INFO', 'ERROR'],
-		default='INFO',
-		help='Set the logging level (default is INFO)',
+		choices=['DEBUG', 'INFO', 'WARNING', 'ERROR'],
+		default='WARNING',
+		help='Set the logging level (default is WARNING)',
 	)
 	parser.add_argument(
 		'--auth-file',
 		'-a',
 		default='./.svo_auth',
 		help='File containing authentication credentials for the SVO (in the format email:API key)',
-	)
-	parser.add_argument(
-		'--min-modif-time',
-		'-m',
-		type=utils.parse_date_time_string,
-		help='Only extract the metadata if the modification time is later than the minimum',
 	)
 	parser.add_argument(
 		'--no-submit',
@@ -91,10 +112,22 @@ if __name__ == '__main__':
 		help='Path to a JSONL file to which the metadata will be written, instead of printed',
 	)
 	parser.add_argument(
-		'fits_files',
-		metavar='FITS FILE',
-		nargs='+',
-		help='Path to a FITS file to process (also accept glob pattern)',
+		'tar_file_path',
+		metavar='TAR-ARCHIVE',
+		type=Path,
+		help='Path to the TAR archive on disk contaning FITS files to process',
+	)
+	parser.add_argument(
+		'--file-url',
+		metavar='URL',
+		required=True,
+		help='URL to download the TAR archive',
+	)
+	parser.add_argument(
+		'--thumbnail-url',
+		metavar='URL',
+		required=True,
+		help='URL to an image (NOT an HTML page)',
 	)
 	args = parser.parse_args()
 
@@ -105,22 +138,26 @@ if __name__ == '__main__':
 	)
 
 	data_location = {
-		'file_url': 'https://lsodb.astro.sk/api/20250912/data/download',
-		'file_size': 11339130880,
-		'file_path': 'lso_comp-s_lev1.0_20250912.tar',
-		'thumbnail_url': 'https://lsodb.astro.sk/api/20250912/CoMP-S_obse_previews/lso_comp-s_lev1.0_20250912_061110_wave0656_pa293_cam2_obse_wavepoint001_all_frames.png',
+		'file_url': args.file_url,
+		'file_size': args.tar_file_path.stat().st_size,
+		'file_path': args.tar_file_path.name,
+		'thumbnail_url': args.thumbnail_url,
 		'offline': False,
 	}
 
+	logging.info('Setting data location to\n\t%s', '\n\t'.join('%s: %s' % item for item in data_location.items()))
+
 	try:
 		provider = Provider(
-			RESTfulApi(auth_file=args.auth_file, debug=args.verbose == 'DEBUG'), DATASET, data_location=data_location
+			RESTfulApi(auth_file=args.auth_file, debug=args.verbose == 'DEBUG'),
+			DATASET,
+			data_location=data_location,
 		)
 	except Exception as error:
 		logging.critical('Could not initialise provider: %s', error)
 		raise
 
-	items = list_items(args.fits_files, args.min_modif_time)
+	items = list_items(args.tar_file_path)
 
 	if args.output_file:
 		with open(args.output_file, 'wt') as output_file:
